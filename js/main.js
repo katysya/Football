320afd2aed8df2172cf0cb3899b4fc9e672ca0c8
katyusya <katusha.fedyakena@mail.ru>
2023-01-13 18:01:54 +0300

    Создание блока News

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+/*Carousel News*/
+
+const gap = 16;
+
+const carousel = document.querySelector(".news__carousel"),
+    content = document.querySelector(".news__content"),
+    next = document.querySelector(".news__next"),
+    prev = document.querySelector(".news__prev");
+
+next.addEventListener("click", e => {
+    carousel.scrollBy(width + gap, 0);
+    if (carousel.scrollWidth !== 0) {
+        prev.style.opacity = "1";
+    }
+    if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.opacity = "0.2";
+    }
+});
+
+prev.addEventListener("click", e => {
+    carousel.scrollBy(-(width + gap), 0);
+    if (carousel.scrollLeft - width - gap <= 0) {
+        prev.style.opacity = "0.2";
+    }
+    if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.opacity = "1";
+    }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
