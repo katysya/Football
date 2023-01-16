95397c2a5d358e79780ca5e2368a4044177ae2fe
katyusya <katusha.fedyakena@mail.ru>
2023-01-16 14:58:05 +0300

    Добавление меню для мобильной версии

--- js/main.js
+++ js/main.js
@@ -76,3 +76,90 @@ prev.addEventListener("click", e => {
 
 let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+/*Scroll*/
+const linkNav = document.querySelectorAll('.header__nav-link');
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        linkNav.forEach(item => {
+            item.classList.remove('header__nav-link--active');
+        });
+
+        link.classList.add('header__nav-link--active');
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = 0;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        burger.classList.toggle('burger--active');
+        nav.classList.toggle('nav--visible');
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
+/*Tabs*/
+const table = document.querySelector('.start-page__statistic');
+const tableTitles = document.querySelectorAll('.start-page__statistic-name');
+const tableContent = document.querySelectorAll('.start-page__statistic-table');
+
+console.log(table);
+console.log(tableTitles);
+console.log(tableContent);
+
+
+function hideTabContent() {
+    tableContent.forEach(item=> {
+        item.style.display = 'none';
+    });
+
+    tableTitles.forEach(item => {
+        item.classList.remove('start-page__statistic-name--active');
+    });
+
+    console.log("сработал Hide");
+}
+
+function showTabContent(i = 0) {
+    console.log("сработал Show");
+    tableTitles[i].classList.add('start-page__statistic-name--active');
+    tableContent[i].style.display = 'block';
+
+}
+
+// hideTabContent();
+// showTabContent();
+
+table.addEventListener('click', function(event) {
+    if(event.target && event.target.classList.contains('start-page__statistic-name')){
+        tableTitles.forEach((item, index) => {
+            if(event.target == item) {
+                hideTabContent();
+                showTabContent(index);
+            }
+        });
+    }
+});
+
+/* Burger Menu*/
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', ()=> {
+
+    console.log('Сработал burger');
+    nav.classList.toggle('nav--visible');
+    burger.classList.toggle('burger--active');
+});
